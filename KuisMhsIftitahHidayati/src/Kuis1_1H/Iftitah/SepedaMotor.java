455b11603e83beba5b39f69a34708295403b6e0d
Your Name <you@example.com>
2021-03-06 20:41:50 +0700

    commited

--- KuisMhsIftitahHidayati/src/Kuis1_1H/Iftitah/SepedaMotor.java
+++ KuisMhsIftitahHidayati/src/Kuis1_1H/Iftitah/SepedaMotor.java
@@ -10,46 +10,72 @@ package Kuis1_1H.Iftitah;
  * @author USER
  */
 public class SepedaMotor {
-    String merk;
-    boolean kontakMotor = false;
-    int kecepatanMotor;
-    
-    SepedaMotor(){
 
+    String merk;
+    boolean kontakMotor;
+    int kecepatanMotor = 0;
+
+    SepedaMotor() {
     }
-    SepedaMotor(String mrk){
+
+    SepedaMotor(String mrk) {
         merk = mrk;
+        kontakMotor = false;
     }
-    void nyalakanMotor(boolean nm){
+
+    void nyalakanMotor(boolean nm) {
         kontakMotor = nm;
     }
-    void matikanMotor (boolean mm){
-        kontakMotor = mm;
-    }
-    void tambahKecepatanMotor(int tam){
-        if (kecepatanMotor == 100) {
-            System.out.println("--------------------------------");
-            System.out.println("Maksimal kecepatan adalah 100 km");
-            System.out.println("--------------------------------");
-        }else{
-            this.kecepatanMotor += tam;
+
+    void matikanMotor(boolean mm) {
+        if (mm == true) {
+            kontakMotor = false;
         }
     }
-    void kurangiKecepatanMotor (int kur){
-        if (kecepatanMotor == 0) {
-            System.out.println("--------------------------------");
-            System.out.println(" Minimal kecepatan adalah 0 km");
-            System.out.println("--------------------------------");
-        }else{
-            this.kecepatanMotor -= kur;
+
+    void tambahKecepatanMotor(int kec) {
+        if (kecepatanMotor >= 100) {
+            System.out.println("|--------------------------------|");
+            System.out.println("|Maksimal kecepatan adalah 100 km|");
+            System.out.println("|--------------------------------");
+        } else {
+            System.out.println("|--------------------------------|");
+            System.out.println("|       Kecepatan ditambah       |");
+            System.out.println("|--------------------------------|");
+            this.kecepatanMotor += kec;
+            System.out.println("| Kecepatan saat ini : " + this.kecepatanMotor+ "         |");
+            System.out.println("|--------------------------------|");
         }
     }
-    void info(){
-        if (kontakMotor==true) {
-            System.out.println("SEPEDA MOTOR TELAH DIHIDUPKAN");
-        } else{
-            System.out.println("SEPEDA MOTOR TELAH DIMATIKAN");
-        } 
-        System.out.println("KECEPATAN : " + kecepatanMotor);
+
+    void kurangiKecepatanMotor(int kec) {
+        if (kecepatanMotor <= 0) {
+            System.out.println("|--------------------------------|");
+            System.out.println("| Minimal kecepatan adalah 0 km  |");
+            System.out.println("|--------------------------------|");
+        } else {
+            System.out.println("|--------------------------------|");
+            System.out.println("|      Kecepatan dikurangi       |");
+            System.out.println("|--------------------------------|");
+            this.kecepatanMotor -= kec;
+            System.out.println("| Kecepatan saat ini : " + this.kecepatanMotor + "         |");
+            System.out.println("|--------------------------------|");
+        }
+    }
+
+    void info() {
+        System.out.println("|--------------------------------|");
+        System.out.println("|   INFO KONDISI SEPEDA MOTOR    |");
+        System.out.println("|--------------------------------|");
+        System.out.println("|Jenis Sepeda Motor : " + merk+"      |");
+        if (kontakMotor == true) {
+            System.out.println("|Sepeda Motor Telah Dihidupkan   |");
+        } else {
+            System.out.println("|Sepeda Motor Kondisi Mati       |");
+            this.kecepatanMotor = 0;
+        }
+        System.out.println("|Kecepatan : " + kecepatanMotor+"                   |");
+        System.out.println("|--------------------------------|");
+        
     }
 }
--- KuisMhsIftitahHidayati/src/Kuis1_1H/Iftitah/SepedaMotorMain.java
+++ KuisMhsIftitahHidayati/src/Kuis1_1H/Iftitah/SepedaMotorMain.java
@@ -5,12 +5,86 @@
  */
 package Kuis1_1H.Iftitah;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
  */
 public class SepedaMotorMain {
+
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        SepedaMotor motor = null;
+        String status;
+        int menu;
+
+        System.out.println("|===============================|");
+        System.out.println("|    Menu Jenis Sepeda Motor    |");
+        System.out.println("|===============================|");
+        System.out.println("| 1. Honda                      |");
+        System.out.println("| 2. Yamaha                     |");
+        System.out.println("| 3. Ducati                     |");
+        System.out.println("| 4. Suzuki                     |");
+        System.out.println("|===============================|");
+        System.out.print("Pilih Jenis : ");
+        int pilih = input.nextInt();
+        if (pilih == 1) {
+            motor = new SepedaMotor("Honda");
+        } else if (pilih == 2) {
+            motor = new SepedaMotor("Yamaha");
+        } else if (pilih == 3) {
+            motor = new SepedaMotor("Ducati");
+        } else if (pilih == 4) {
+            motor = new SepedaMotor("Suzuki");
+        }
+        System.out.println("");
+
+        System.out.println("|-------------------------------|");
+        System.out.println("|   Pilih Kondisi Sepeda Motor  |");
+        System.out.println("|-------------------------------|");
+        System.out.println("| 1. Nyalakan Mesin             |");
+        System.out.println("| 2. Matikan Mesin              |");
+        System.out.println("|-------------------------------|");
+        System.out.print("Pilih Kondisi: ");
+        int pilih2 = input.nextInt();
+        System.out.println("");
+        if (pilih2 == 1) {
+
+            do {
+                motor.nyalakanMotor(true);
+                System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+                System.out.println("|   Pilih Kondisi Selanjutnya   |");
+                System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+                System.out.println("| 1. Tambahkan Kecepatan        |");
+                System.out.println("| 2. Kurangi Kecepatan          |");
+                System.out.println("| 3. Matikan Mesin              |");
+                System.out.println("| 4. Info                       |");
+                System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+                System.out.print("Pilih Menu : ");
+                menu = input.nextInt();
+                System.out.println("");
+                if (menu == 1) {
+                    motor.tambahKecepatanMotor(5);
+                } else if (menu == 2) {
+                    motor.kurangiKecepatanMotor(5);
+                } else if (menu == 3) {
+                    motor.matikanMotor(true);
+                    motor.info();
+                    break;
+                } else if (menu == 4) {
+                    motor.info();
+                }
+                input.nextLine();
+                System.out.print("Ingin berhenti? (Y/T) : ");
+                status = input.nextLine();
+                System.out.println("");
+            } while ("T".equals(status));
+
+        } else {
+            motor.info();
+            System.out.println("| Hidupkan Motor terlebih dahulu |");
+            System.out.println("|--------------------------------|");
+        }
     }
 }
